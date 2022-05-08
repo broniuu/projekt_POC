fd7a5ff3672f85bc982f28a234d11259a6da0f68
34308 <arcade@spoko.pl>
2022-05-08 10:34:29 +0200

    dziala razem z wypalaniem aplikacji teoretycznie powinno dzilac nawet na linuxie

---
+++ src/main/java/com/example/projekt_poc/Main_1.java
@@ -0,0 +1,39 @@
+package com.example.projekt_poc;
+
+import com.example.projekt_poc.HelloApplication;
+import org.opencv.core.Core;
+
+import java.io.File;
+import java.io.InputStream;
+
+public class Main_1 {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+        loadLibraries();
+    }
+   private static void loadLibraries() {
+
+        try {
+            InputStream in = null;
+            File fileOut = null;
+            String osName = System.getProperty("os.name");
+            String opencvpath = System.getProperty("user.dir");
+            if(osName.startsWith("Windows")) {
+                int bitness = Integer.parseInt(System.getProperty("sun.arch.data.model"));
+
+                if (bitness == 64) {
+                    opencvpath=opencvpath+"/src/java/x64/";
+                } else {
+                    opencvpath=opencvpath+"/src/java/x86/";
+                }
+            }
+            else if(osName.equals("Mac OS X")){
+                opencvpath = opencvpath+"Your path to .dylib";
+            }
+            System.out.println(opencvpath);
+            System.load(opencvpath + Core.NATIVE_LIBRARY_NAME + ".dll");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load opencv native library", e);
+        }
+    }
+}
